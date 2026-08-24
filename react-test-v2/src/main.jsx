3a8997e99977bc988b9bab3852038616b4b1005b
Ricardo_Sanchez <accel.diamond469@gmail.com>
2026-08-24 16:35:12 -0600

    Retry Fix

--- react-test-v2/src/main.jsx
+++ react-test-v2/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import heroImg from "./assets/hero.png";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+const Home = "/react-test-v2";
+
 createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/ReactTestV2">
         <Routes>
