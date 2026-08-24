d72876d1c828e86904765f0cb17a9554c94efe9c
Ricardo_Sanchez <accel.diamond469@gmail.com>
2026-08-24 16:32:58 -0600

    BrowserRouter fix

--- react-test-v2/src/main.jsx
+++ react-test-v2/src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import heroImg from "./assets/hero.png";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
+    <BrowserRouter basename="/ReactTestV2">
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>,
   <StrictMode>
     <App />
   </StrictMode>,
